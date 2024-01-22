e988a6239a384a9f3e74494a06f4abff15bf648e
Robert <robert.aleks.zuk@gmail.com>
2024-01-22 20:10:07 +0100

    ref code 1

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -19,7 +19,7 @@ export default async function DashboardLayout({
       <main className="md:pl-56 pt-[80px] pb-[20px] h-dvh min-h-[100vh] bg-background">
         {children}
       </main>
-      <footer className="w-full  flex justify-center flex-wrap items-center md:pl-56 h-[100px] bg-background border-t text-sm">
+      <footer className="absolute left-0 z-50 w-full  flex justify-center flex-wrap items-center md:pl-56 h-[100px] bg-background border-t text-sm">
         <p className="">{`Copyright ©${new Date().getFullYear()}`}&nbsp;</p>
         <a href="https://robertzuk.pl">Robet Żuk&nbsp;</a>
         <p> All Rights Reserved.</p>
