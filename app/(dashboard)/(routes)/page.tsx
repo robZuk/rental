047d9143ea530806c8ba4189ee99aa2d472de1c9
Robert <robert.aleks.zuk@gmail.com>
2024-02-20 14:06:32 +0100

    add intro animation

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import Hero from "../../../public/hero1.jpg";
 
 const DashboardPage = () => {
+  // await new Promise((resolve) => {
+  //   setTimeout(resolve, 2000);
+  // });
+
   return (
     <div className="relative">
       <div
@@ -15,27 +20,18 @@ const DashboardPage = () => {
       >
         <Image
           fill
-          src="/hero1.jpg"
+          src={Hero}
           alt="hero"
           className="absolute top-0 object-cover"
           style={{
             filter: "contrast(.8) brightness(.9) grayscale(.5) saturate(1.2)",
             objectPosition: "20% 10%",
           }}
+          placeholder="blur"
+          priority
         />
       </div>
-      {/* <video
-        src="/video1.mp4"
-        autoPlay
-        loop
-        muted
-        className="w-full object-cover absolute top-0"
-        style={{
-          height: "calc(100vh - 80px)",
-          filter: "contrast(1.1)",
-        }}
-      /> */}
-      <div className="absolute text-white top-[20vh] right-[8vw] p-8">
+      <div className="absolute text-white top-[20vh] right-[8vw] p-8 intro">
         <div>
           <p className="text-5xl sm:text-7xl font-extrabold">Build with us!</p>
           <p className="font-bold text-xl">
