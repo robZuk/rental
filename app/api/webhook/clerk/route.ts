bbb938f4c0ca72feafda2fe0d875d53d6666f6c6
Robert <robert.aleks.zuk@gmail.com>
2024-02-23 12:08:41 +0100

    clerk webhook

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -10,9 +10,9 @@ import { Webhook } from "svix";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const CLERK_WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
-  if (!WEBHOOK_SECRET) {
+  if (!CLERK_WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
     );
@@ -66,7 +66,7 @@ export async function POST(req: Request) {
     const user = {
       userId: id,
       email: email_addresses[0].email_address,
-      username: username!,
+      //   username: username!,
       firstName: first_name,
       lastName: last_name,
       imageUrl: image_url,
