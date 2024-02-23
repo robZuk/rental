02c65485075c0cb1bb61c0f695e4b077340aad2d
Robert <robert.aleks.zuk@gmail.com>
2024-02-23 11:28:40 +0100

    clerk webhook

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -66,7 +66,7 @@ export async function POST(req: Request) {
     const user = {
       userId: id,
       email: email_addresses[0].email_address,
-      //   username: username!,
+      username: username!,
       firstName: first_name,
       lastName: last_name,
       imageUrl: image_url,
