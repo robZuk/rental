3ff119e01aec0feba431e2db698ff9e23926f54b
Robert <robert.aleks.zuk@gmail.com>
2024-02-23 11:42:14 +0100

    clerk webhook

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/categories", "/api/uploadthing", "/api/webhook"],
+  publicRoutes: [
+    "/",
+    "/categories",
+    "/api/uploadthing",
+    "/api/webhook",
+    "/api/webhooks/clerk",
+    "/api/webhooks/stripe",
+  ],
 });
 
 export const config = {
