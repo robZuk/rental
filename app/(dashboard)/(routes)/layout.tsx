25599f12f77f22d58687eb53e346396f2b2ea475
Robert <robert.aleks.zuk@gmail.com>
2024-04-26 10:44:59 +0200

    add RouteLayout

---
+++ app/(dashboard)/(routes)/layout.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useEffect } from "react";
+import useCart from "@/hooks/use-cart";
+import { useAuth } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+
+function RoutesLayuot({ children }: { children: React.ReactNode }) {
+  const cart = useCart();
+  const { userId } = useAuth();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    !userId && cart.removeAll();
+  }, [userId]);
+
+  return (
+    <div
+      style={{
+        paddingBottom: pathname === "/" ? "0px" : "20px",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default RoutesLayuot;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,27 +1,8 @@
-"use client";
 import { Navbar } from "./_components/navbar";
 import { Sidebar } from "./_components/sidebar";
 import { Footer } from "./_components/footer";
-import { usePathname } from "next/navigation";
-import { useAuth } from "@clerk/nextjs";
-import { useEffect } from "react";
-import useCart from "@/hooks/use-cart";
 
-export default function DashboardLayout({
-  children,
-}: {
-  children: React.ReactNode;
-  params: { storeId: string };
-}) {
-  const cart = useCart();
-  const pathname = usePathname();
-  const { userId } = useAuth();
-
-  //clera cart after SignOut
-  useEffect(() => {
-    !userId && cart.removeAll();
-  }, [userId]);
-
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full bg-background pb-8">
       <div className="h-[80px] md:pl-56 fixed w-full z-50">
@@ -30,12 +11,7 @@ export default function DashboardLayout({
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
-      <main
-        className="md:pl-56 pt-[80px] h-dvh min-h-[100vh] bg-background"
-        style={{
-          paddingBottom: pathname === "/" ? "0px" : "20px",
-        }}
-      >
+      <main className="md:pl-56 pt-[80px] h-dvh min-h-[100vh] bg-background">
         {children}
       </main>
       <Footer />
