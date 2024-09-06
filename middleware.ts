816c8663c23d6c9a2eef67176cfdb29558bc6f26
Robert <robert.aleks.zuk@gmail.com>
2024-09-06 11:24:33 +0200

    modified public routes

--- middleware.ts
+++ middleware.ts
@@ -11,11 +11,11 @@ export default authMiddleware({
     "/api/webhooks",
     "/api/webhooks/clerk",
     "/api/webhooks/stripe",
+    "/:catchall",
   ],
 });
 
 export const config = {
-  // matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
